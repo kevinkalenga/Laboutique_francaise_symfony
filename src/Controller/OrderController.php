bcba2999304486a2178740f609276c099b749436
kevin kalenga <kevinkalenga10@gmail.com>
2025-05-25 17:51:54 +0200

    Sommaire de la commande et ajout de style

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\OrderTypeForm;
+use App\Classe\Cart;
 
 final class OrderController extends AbstractController
 {
@@ -21,11 +24,48 @@ final class OrderController extends AbstractController
 
         $form = $this->createForm(OrderTypeForm::class, null, [
             'addresses' => $addresses,
-            // 'action' => $this->generateUrl('app_order_summary')
+            'action' => $this->generateUrl('app_order_summary')
         ]);
 
         return $this->render('order/index.html.twig', [
             'deliverForm' => $form->createView(),
         ]);
-    }  
+    } 
+    
+    
+
+
+     /*
+    * 2eme etape du tunel d'achat
+    * Récap de la commande de l'utilisateur
+    * Insertion en base de donnée en bdd, voila pk la fonction add
+      Préparation du paiement vers Stripe
+    */
+    #[Route('/commande/recapitulatif', name: 'app_order_summary')]
+    public function add(Request $request,  Cart $cart, EntityManagerInterface $entityManager): Response
+    {
+        
+        if ($request->getMethod() != 'POST') {
+            return $this->redirectToRoute('app_cart');
+        } 
+        
+        $form = $this->createForm(OrderTypeForm::class, null, [
+            'addresses' => $this->getUser()->getAddresses(),
+            
+        ]);
+
+        $form->handleRequest($request); 
+
+        if($form->isSubmitted() && $form->isValid()) {
+            //  dd($form->getData());
+        }
+
+        
+        return $this->render('order/summary.html.twig', [
+             'choices' => $form->getData(),
+             'cart' => $cart->getCart(),
+             'totalWt' => $cart->getTotalWt()
+        ]);
+    }
+
 }
