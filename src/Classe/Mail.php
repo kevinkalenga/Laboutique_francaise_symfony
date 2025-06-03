c6689a35fedf21c43965e41b0b9db3817209cf64
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-03 14:22:31 +0200

    Création d'une classe Mail et envoie de notre premier mail

---
+++ src/Classe/Mail.php
@@ -0,0 +1,40 @@
+<?php 
+
+namespace App\Classe; 
+use Mailjet\Client;
+use Mailjet\Resources;
+
+class Mail 
+{
+    public function send($to_email, $to_name, $subject, $content)
+    {
+        
+         $mj = new Client($_ENV['MJ_APIKEY_PUBLIC'], $_ENV['MJ_APIKEY_PRIVATE'], true, ['version' => 'v3.1']);
+        
+        $body = [
+    'Messages' => [
+        [
+            'From' => [
+                'Email' => "nathanaelkalenga2@gmail.com",
+                'Name' => "La boutique française"
+            ],
+            'To' => [
+                [
+                    'Email' => $to_email,
+                    'Name' => $to_name
+                ]
+            ],
+            'Subject' => $subject,
+            'TextPart' => "Greetings from Mailjet!",
+            'HTMLPart' => $content
+        ]
+    ]
+];
+   
+    // Envoi de l'email 
+    $response = $mj->post(Resources::$Email, ['body' => $body]);
+
+   
+
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Mail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,6 +12,11 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        
+       
+       $mail = new Mail();
+       $mail->send('kevinkalenga10@gmail.com', 'Yves', 'Bonjour, teste de ma class', 'contenu');
+        
         return $this->render('home/index.html.twig');
     }
 }
